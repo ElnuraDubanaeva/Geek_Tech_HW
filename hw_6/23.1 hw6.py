9fe236c9d1cd4da5627fe98e5510b674dd1bb723
ElnuraDubanaeva <elena040203@gmail.com>
2022-10-23 18:12:26 +0600

    Update 23.1 hw2.py

--- hw_6/23.1 hw6.py
+++ hw_6/23.1 hw6.py
@@ -4,32 +4,36 @@ with open('data.txt', 'r', encoding='UTF - 8') as data:
     result = data.read()
 
 
+def names():
+    name_list = re.findall(r'(?:\b[A-Z][a-zA-Z\'\-\. ]+[\s]+[a-zA-Z\'\-\. ]+\b)', result)
+    with open('names.txt', 'w', encoding='UTF-8') as name:
+        name.writelines('%s\n' % item for item in name_list)
+        name.close()
+        return f'created a file called names.txt'
+
+
 def colors():
     color_list = re.findall(r'(?:#\S*)', result)
     with open('colors.txt', 'w', encoding='UTF-8') as color:
         color.writelines('%s\n' % item for item in color_list)
         color.close()
+        return f'created a file called colors.txt'
 
 
 def emails():
-    email_file = re.findall(r'(?:\w*@\S+\.\w+|\.\w+&)', result)
+    email_list = re.findall(r'(?:\w*@\S+\.\w+|\.\w+&)', result)
     with open('emails.txt', 'w', encoding='UTF-8') as email:
-        email.writelines('%s\n' % item for item in email_file)
+        email.writelines('%s\n' % item for item in email_list)
         email.close()
+        return f'created a file called emails.txt'
 
 
 def files():
     file_list = re.findall(r'(?:\s\w+\.\w+)', result)
-    with open('files.txt', 'w', encoding='UTF-8') as gmail:
-        gmail.writelines('%s\n' % item for item in file_list)
-        gmail.close()
-
-
-def names():
-    name_list = re.findall(r'(?:^\S*\s\S*)', result)
-    with open('names.txt', 'w', encoding='UTF-8') as name:
-        name.writelines('%s\n' % item for item in name_list)
-        name.close()
+    with open('files.txt', 'w', encoding='UTF-8') as file:
+        file.writelines('%s\n' % item for item in file_list)
+        file.close()
+        return f'created a file called files.txt'
 
 
 def show_info():
@@ -37,23 +41,24 @@ def show_info():
 
 
 def choice():
-    if n == 1:
-        names()
-    elif n == 2:
-        emails()
-    elif n == 3:
-        files()
-    else:
-        colors()
-
-
-print(show_info())
-global n
-n = int(input('Введите число:'))
-while True:
-    if n == 5:
-        break
-    else:
-        print(show_info())
+    print(show_info())
+    while True:
         n = int(input('Введите число:'))
-        choice()
+        if n == 1:
+            print(names())
+            print(show_info())
+        elif n == 2:
+            print(emails())
+            print(show_info())
+        elif n == 3:
+            print(files())
+            print(show_info())
+        elif n == 4:
+            print(colors())
+            print(show_info())
+        else:
+            print('Exit')
+            break
+
+
+choice()
